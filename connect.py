b29d6de0d3ad49b5da67ca48e4b27c4a7b0d6f7e
Evra-Kaze <98854089+Evra-Kaze@users.noreply.github.com>
2026-09-15 04:33:30 -0400

    a

--- connect.py
+++ connect.py
@@ -2,3 +2,18 @@
 # If you know the password to a WiFi network and are just in a shell, 
 # you would need to use things like wpa_supplicant + dhclient, nmcli, a bunch of other tools.
 # So, tell the wifi, the password, and just connect to it using this script, thus not having to create any config...
+
+import argparse
+import os
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass
